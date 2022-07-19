750c5d9a190b1af60fabf7b98a7cd1c40c99ef51
Kodama <fk@fkodama.com>
2022-07-19 15:23:34 -0400

    Rewrite to firebase version 9

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from 'react-bootstrap'
 import AuthProvider from "../contexts/AuthContext"
-
+import { useUserContext } from '../contexts/AuthContext'
 function App() {
+
+  const { loading, error, currentUser } = useUserContext()
+
   return (
       <AuthProvider>
         <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}}>
           <div className="w-100" style={{ maxWidth: "400px"}}>
+            {error && <p className='error'>{error}</p>}
+            {loading ? <h2>Loading...</h2> : <> {currentUser ? <Dashboard /> : <Auth />}</>}
+
             <Signup />
           </div>
         </Container>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,36 +1,68 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { onAuthStateChanged, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '../firebase'
 
 
-const AuthContext = React.createContext()
+const AuthContext = createContext({})
 
-export function useAuth() {
-    return useContext(AuthContext)
-}
+export const useUserContext = () => useContext(AuthContext)
 
 const AuthProvider = ({children}) => {
 
-    const [currentUser, setCurrentUser ] = useState()
-
-    function signup(email, password) {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
-
+    const [currentUser, setCurrentUser ] = useState(null)
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState('')
+ 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, user => {
-            setCurrentUser(user)
-        })
-        return unsubscribe
+      setLoading(true)
+      const unsubscribe = onAuthStateChanged(auth, user => {
+        user ? setCurrentUser(user) : setCurrentUser(null)
+        setError('')
+        setLoading(false)
+      })
+      return unsubscribe
     }, [])
 
-    const value = {
+    const signUpUser = (email, password) => {
+        setLoading(true)
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          return updateProfile(auth.currentUser, {
+            displayName: name,
+          })
+        }).then((res) => console.log(res))
+          .catch((err) => setError(err.message))
+          .finally(() => setLoading(false))
+    }
+
+    const signInUser = (email, password) => {
+      setLoading(true)
+      signInWithEmailAndPassword(auth, email, password)
+        .then((res) => console.log(res))
+        .catch((err) => setError(err))
+        .finally(() => setLoading(false))
+    }
+
+    const logOutUser = () => {
+      signOut(auth)
+    }
+
+    const forgotPassword = (email) => {
+      return sendPasswordResetEmail(auth, email)
+    }   
+
+    const contextValue = {
         currentUser,
-        signup
+        loading,
+        error,
+        signUpUser,
+        signInUser,
+        logOutUser,
+        forgotPassword,
     }
 
   return (
-    <AuthContext.Provider value={value}>
+    <AuthContext.Provider value={contextValue}>
       {children}
     </AuthContext.Provider>
   )
